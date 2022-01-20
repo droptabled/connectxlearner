f099114a0640dd9642bebcdc721e8ea93a84fef3
Mike <minghan.mike.li@gmail.com>
2022-01-19 23:58:54 -0500

    neural net setup

--- app/modellers/standard_builder.rb
+++ app/modellers/standard_builder.rb
@@ -2,15 +2,33 @@
 
 class StandardBuilder
   def generateInputNodes(game:, bot:)
+    new_nodes = []
     game.width.times do |x|
       game.height.times do |y|
-        TransferNode.create(layer: 0, bot: bot)
+        new_nodes << { layer: 0, bot: bot }
       end
     end
+    
+    TransferNode.insert_all(new_nodes)
   end
 
   def generateAndMapLayerNodes(bot:, parent_layer:, count:)
     parent_nodes = TransferNode.where(bot: bot, layer: parent_layer)
-    #new_nodes = TransferNode.
+    new_layer = parent_layer + 1
+    new_nodes = []
+    parent_nodes.map do |node|
+      count.times do |count|
+        new_nodes << { bot: bot, layer: new_layer }
+      end
+    end
+    TransferNode.insert_all(new_nodes)
+
+    new_edges = []
+    parent_nodes.map do |node|
+      new_nodes.map do |new_node|
+        new_edges << { weight: 0, upstream_node: node, downstream_node: new_node}
+      end
+    end
+    TramsferEdge.insert_all(new_edges)
   end
 end
