01412d27baf535d91d9c093b38e2f92aee2b2f98
Mike <minghan.mike.li@gmail.com>
2022-02-06 23:05:23 -0500

    remove all sprockets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :games do
-    resources :bots do
-      resources :play, only: :index
-    end
+    resources :bots
   end
+
+  resources :play, only: :index
+
   root controller: :games, action: :index
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
