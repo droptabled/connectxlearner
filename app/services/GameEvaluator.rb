e03936ff7c48373123995ce613617d46dcf340c7
Mike <minghan.mike.li@gmail.com>
2022-02-18 23:52:45 -0500

    victory check

--- app/services/GameEvaluator.rb
+++ app/services/GameEvaluator.rb
@@ -21,17 +21,40 @@ class GameEvaluator
 
     def checkVictory(row:, column:)
       raise StandardError.new("No player token") if game_array[height, width] == 0
-
+      playerId = game_array[row, column]
       # Vertical
-
+      checkDirections(row: row, column: column, vertical: 1)  ||
       # Horizontal
-
+      checkDirections(row: row, column: column, horizontal: 1) ||
       # Diagonal NE - SW
-
+      checkDirections(row: row, column: column, horizontal: 1, vertical: 1) ||
       # Diagonal SE - NW
+      checkDirections(row: row, column: column, horizontal: 1, vertical: -1)
+    end
+
+    def checkDirections(row:, column:, horizontal:0, vertical:0)
+      first = checkDirection(row: row, column: column, horizontal: horizontal, vertical: vertical)
+      second = checkDirection(row: row, column: column, horizontal: horizontal * -1, vertical: vertical * -1, count: 3-north)
+      if north + south >= 3
+        return playerId
+      else
+        nil
+      end
     end
 
     # Horizontal -1 (West), 1 (East)
     # Vertical -1 (South), 1 (North)
-    def checkDirection(horizontal:, vertical:)
+    def checkDirection(row:, column:, horizontal:0, vertical:0, count: 4)
+      playerId = game_array[row, column]
+      playerDirection = 0
+      count.times do |n|
+        if game_array[row + n*vertical, column + n*horizontal] == playerId
+          playerDirection += 1
+        else
+          return playerDirection
+        end
+      end
+
+      playerDirection
+    end
 end
