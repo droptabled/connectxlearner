da1a3956c7ff6e2735d03f7cdbec8383d4d8b406
Mike <minghan.mike.li@gmail.com>
2022-07-04 15:02:52 -0400

    custom inspect on long layer matrix arrays

--- app/models/transfer_layer.rb
+++ app/models/transfer_layer.rb
@@ -5,6 +5,26 @@ require 'nmatrix'
 class TransferLayer < ApplicationRecord
   belongs_to :bot
 
+  # Overwrite inspect on transfer_layer to not show the giant layer matrix
+  def inspect
+    inspection = self.class.attribute_names.map do |name|
+      case name
+      when "layer_matrix"
+        layer = layer_matrix
+        dim_str = ""
+        while layer.instance_of?(Array)
+          dim_str += "[#{layer.size}]"
+          layer = layer[0]
+        end
+        "#{name}: #{layer_matrix.class}" + dim_str
+      else
+        "#{name}: #{attribute_for_inspect(name)}"
+      end
+    end.compact.join(", ")
+
+    "#<#{self.class} #{inspection}>"
+  end
+
   def node_array
     @node_array ||= NMatrix.new([row_count, col_count], layer_matrix)
   end
